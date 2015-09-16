03a57bf517cf37b0a1ceb82432ff9b95eaea529c
Will Sewell <me@willsewell.com>
2015-09-16 17:48:19 +0100

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,6 @@
+# A sample Gemfile
+source "https://rubygems.org"
+
+gem "sonos", :github => "mdpye/sonos-ruby"
+gem "pusher-client"
+gem "excon"
---
+++ client.rb
@@ -0,0 +1,112 @@
+require 'rubygems'
+require 'sonos'
+require 'pusher-client'
+require 'excon'
+require 'json'
+
+system = Sonos::System.new # Auto-discovers the system
+
+if system.speakers.empty?
+  abort('No Sonos speaker found!')
+end
+
+speaker = system.speakers.first
+p speaker
+
+http = Excon.new('https://slonos.herokuapp.com')
+
+# def send_data(data)
+#   http.post(
+#     :headers => { "Content-Type" => "application-json" },
+#     :body => JSON.generate(data),
+#   )
+# end
+
+APP_KEY = ENV['PUSHER_KEY']
+CLIENT_TOKEN = ENV['CLIENT_TOKEN']
+
+auth_method = lambda do |socket_id, channel|
+  response = http.post({
+    :path => '/pusher_auth',
+    :body => URI.encode_www_form(:socket_id => socket_id, :channel => channel.name, :client_token => CLIENT_TOKEN),
+    :headers => { "Content-Type" => "application/x-www-form-urlencoded" }
+  })
+  decoded = JSON.parse(response.body) ## TODO validate this worked!
+  decoded['auth']
+end
+
+options = {
+  secure: true,
+  auth_method: auth_method
+}
+pusher = PusherClient::Socket.new(APP_KEY, options)
+pusher.subscribe('private-commands')
+chan = pusher['private-commands']
+
+# Controls
+
+chan.bind('pause') do
+  speaker.pause
+end
+
+chan.bind('play') do
+  speaker.play
+end
+
+chan.bind('volume-up') do
+  speaker.volume += 5
+end
+
+chan.bind('volume-down') do
+  if speaker.volume > 5
+    speaker.volume -= 5
+  else
+    speaker.volume = 0
+  end
+end
+
+chan.bind('add') do |data|
+  decoded = JSON.parse(data) ## TODO validate this worked!
+  speaker.add_spotify_to_queue({ :id => decoded['id'] + '?sid=9&amp;flags=0', :title => decoded['title'] }) # No idea why we need that string, but otherwise we get a 800 response from Sonos
+  if speaker.queue[:items].length == 1 # added the first song, play!
+    speaker.play
+  end
+end
+
+chan.bind('remove') do |data|
+  decoded = JSON.parse(data)
+  queue = speaker.queue[:items]
+  if decoded.has_key?('queue_id')
+    if queue.select { |track| track[:queue_id] == decoded['queue_id'] }.empty?
+      # TODO Send error
+    else
+      speaker.remove_from_queue(decoded['queue_id'])
+    end
+  else
+    speaker.remove_from_queue(queue.last[:queue_id])
+  end
+end
+
+chan.bind('clear-queue') do
+  speaker.clear_queue
+end
+
+# Queries
+
+chan.bind('now-playing') do
+  track = speaker.now_playing
+  track.delete_if {|key, _| ![:title, :artist].include?(key) }
+  p track
+  # send_data(queue[..10])
+end
+
+chan.bind('queue') do
+  queue = speaker.queue[:items]
+  queue.map do |track|
+    track.delete_if {|key, _| ![:queue_id, :title, :artist].include?(key) }
+  end
+  p queue
+  # send_data(queue[..10])
+end
+
+pusher.connect
